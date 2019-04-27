68d0a01dbbd4733370b6ebf1b845f528534858cb
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-04-27 10:17:33 -0400

    trying to just deploy

--- src/components/DumbClick/index.tsx
+++ src/components/DumbClick/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 // import { className } from 'postcss-selector-parser';
 // import { Module } from 'module';
 // import pic from "../../images/bro"
-import { CardMedia } from '@material-ui/core';
+// import { CardMedia } from '@material-ui/core';
 // import 'materialize-css/dist/css/materialize.min.css'
 // import M from 'materialize-css/dist/js/materialize.min.js'
-import Grid from "@material-ui/core/Grid"
 
 interface Props {
     handleClick: CallableFunction, 
@@ -17,18 +16,17 @@ interface Props {
 const DumbClick: React.FunctionComponent<Props> = (props) => {
     // console.log(props);
        return (
-            <Grid item className="col l4">
-                <CardMedia 
-                className="responsive-img"
-                image="picture" 
+            <div className="col l4">
+                <img 
+                className="responsive-img" 
                 src={props.src} 
-                // alt="Click on this to play"
-                // data={props.data}
+                alt="Click on this to play"
+                // data: any={props.data}
                 onClick={(event) => {
                     props.handleClick(JSON.stringify(props.data))
                     }}>
-                </CardMedia>
-            </Grid>
+                </img>
+            </div>
        );
    };
 
